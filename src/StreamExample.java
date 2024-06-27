aeb7ccbb9b759bf251e5f661df0739e8e543cabf
datky <tkachenko.d@cdek.ru>
2024-06-27 21:34:54 +0300

    commit

---
+++ src/StreamExample.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamExample {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(12);
+        list.add(23);
+        list.add(120);
+        list.add(450);
+        list.add(1200);
+        list.add(10000);
+        list.add(1_000_000);
+    }
+}
