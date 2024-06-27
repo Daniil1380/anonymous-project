1903347083c3959c45766391175d34954868dd84
datky <tkachenko.d@cdek.ru>
2024-06-27 20:08:25 +0300

    Initial commit

---
+++ src/FunctionMain.java
@@ -0,0 +1,11 @@
+import java.util.function.Predicate;
+
+public class FunctionMain {
+
+    public static void main(String[] args) {
+        Button a = () -> System.out.println(1);
+        Predicate<>
+
+
+    }
+}
